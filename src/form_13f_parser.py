20e14a2190cca7e1eb70717131463f0798317a9b
Lieven Govaerts <lgo@mobsol.be>
2021-04-19 23:29:18 +0200

    Implemented parser for three table formats, added Excel file with a couple of cik's.

--- src/form_13f_parser.py
+++ src/form_13f_parser.py
@@ -1,16 +1,16 @@
+import os
+import tempfile
 import xml.etree.ElementTree as ET
+from glob import glob
+import pandas as pd
 
-def parse_submission_file(file_path):
+def parse_submission_xml_file(text):
     """
     Opens a file that was originally downloaded from the Edgar site and parse it.
     We assume that the file is a 13F-HR form
     :param file_name:
     :return: a dictionary with all relevant attributes from the document
     """
-
-    # Read all file content in memory
-    with open(file_path) as f:
-        text = f.read()
 
     # Cut the text into blocks (lines) that start right after the <XML> tag
     result = text.split('<XML>')
@@ -21,6 +21,10 @@ def parse_submission_file(file_path):
     # Cut of the unneeded text right after the xml document.
     result = [line.split("</XML>")[0].strip() for line in result]
 
+    # Does the file contain any XML blocks?
+    if len(result) == 0:
+        return None
+
     # Combine the multiple xml documents into one:
     # <form>
     #    <edgarSubmission>...</edgarSubmission>
@@ -30,10 +34,128 @@ def parse_submission_file(file_path):
     for line in result:
         form_node.insert(0, ET.fromstring(line))
 
-    return form_node
+    # parsed_data is an xml ElementTree element, so we can use xpath to browse the
+    # tree
+    ns = { "ns1": "http://www.sec.gov/edgar/thirteenffiler"}
+    cik = form_node.find('.//ns1:edgarSubmission/ns1:headerData/ns1:filerInfo/ns1:filer/ns1:credentials/ns1:cik', ns).text
+
+    # List the companies the investment fund is investing in
+    ns = {"ns1": "http://www.sec.gov/edgar/document/thirteenf/informationtable"}
+    investment_els = form_node.findall('.//ns1:informationTable/ns1:infoTable', ns)
+
+    investments = [parse_investment_from_xml(cik, i) for i in investment_els]
+
+    return investments
 
 
-def parse_investment(investment_el):
+def parse_cid_from_table(accession_number):
+    """
+    Parse the cik number from the accession_number.
+    Example: 0000885709-12-000010 -> 0000885709
+    :param accession_number:
+    :return: <string> cik
+    """
+    return accession_number.strip().split('-')[0]
+
+
+def parse_submission_table_file(text):
+    table_lines = []
+    reading_table = False
+    table_df = None
+    cik = None
+
+    for line in text.splitlines():
+        if line.startswith('<'):
+            if reading_table:
+                if line.startswith('<S>'):
+                    continue
+                # We were reading the lines of the fixed-width table
+                # pandas.read_fwf can only read from a file, not from a text buffer.
+                # write the text to a temp file first.
+                fd, p = tempfile.mkstemp()
+                with os.fdopen(fd, 'w') as f:
+                    f.write('\n'.join(table_lines))
+                    f.write('\n')
+                table_df = pd.read_fwf(p)
+                break
+            continue
+
+        if reading_table:
+            table_lines.append(line)
+            continue
+
+        # Parse all the key:value pairs we are interested in
+        comps = line.strip().split(':')
+        if comps[0] == "ACCESSION NUMBER":
+            cik = parse_cid_from_table(comps[1])
+            continue
+        # Skip key:value pairs we are not interested in
+        if len(comps) > 1:
+            continue
+
+        if len(comps) == 1 and \
+            (comps[0].strip().startswith("SECURITY ") or
+             comps[0].strip().startswith('Name of Issuer ')):
+
+            reading_table = True
+            # Add the header line
+            table_lines.append(line)
+            continue
+
+    if not table_df is None:
+        # Strip all unneeded spaces from the column names
+        table_df.columns = [' '.join(col.strip().split()) for col in table_df.columns]
+        # Try to identify unnamed columns
+        unnamed_cols = [col for col in table_df.columns if col.startswith('Unnamed')]
+        for col in unnamed_cols:
+            if "SH" in table_df[col].to_list():
+                table_df.rename({col:'sshPrnamtType'})
+
+        if 'PRN AMT PRN' in table_df.columns:
+            new = table_df['PRN AMT PRN'].str.split(' ', n = 1, expand = True)
+            table_df['sshPrnamt'] = new[0]
+            table_df['sshPrnamtType'] = new[1]
+
+        # Now rename the columns to what we are using internally. This is a bit tricky,
+        # as in some files there are extra unnamed columns.
+        COL_LIST = ['nameOfIssuer', 'titleOfClass', 'cusip', 'value', 'sshPrnamt',
+                    'sshPrnamtType' ,'cik']
+        table_df = table_df.rename(columns={"SECURITY":"nameOfIssuer",
+                                            "Name of Issuer": "nameOfIssuer",
+                                            "TYPE":"titleOfClass",
+                                            "Class": "titleOfClass",
+                                            "CUSIP":"cusip",
+                                            "MARKET":"value",
+                                            "(x$1000)": "value",
+                                            "SHARES":"sshPrnamt",
+                                            "OPTIONTYPE":"sshPrnamtType"})
+        table_df = table_df.loc[:, table_df.columns.isin(COL_LIST)]
+        table_df["cik"] = cik
+
+        # Delete the table header separator row.
+        if 'titleOfClass' in table_df.columns:
+            table_df = table_df[~table_df['titleOfClass'].astype(str).str.startswith('----')]
+        else:
+            print("Error: table not in recognized format!")
+            return None
+
+        return table_df.to_dict('records')
+
+    return None
+
+
+def parse_submission_file(file_path):
+    # Read all file content in memory
+    with open(file_path) as f:
+        text = f.read()
+
+    if '<?xml version="1.0"' in text:
+        return parse_submission_xml_file(text)
+    else:
+        return parse_submission_table_file(text)
+
+
+def parse_investment_from_xml(cik, investment_el):
     """
     Parse an informationTable/infoTable xml element in a dictionary with the values
     we will use as elements.
@@ -48,10 +170,32 @@ def parse_investment(investment_el):
     value = investment_el.find('ns1:value', ns).text
     sshPrnamt = investment_el.find('ns1:shrsOrPrnAmt/ns1:sshPrnamt', ns).text
     sshPrnamtType = investment_el.find('ns1:shrsOrPrnAmt/ns1:sshPrnamtType', ns).text
-    return {"nameOfIssuer": nameOfIssuer,
+    return {"cik": cik,
+            "nameOfIssuer": nameOfIssuer,
             "titleOfClass": titleOfClass,
             "cusip": cusip,
             "value": value,
             "sshPrnamt": sshPrnamt,
             "sshPrnamtType": sshPrnamtType
             }
+
+
+def parse_all_13f_submission_files(root_folder):
+    lst = []
+    for subm_file in glob(os.path.join(root_folder, '**/full-submission.txt'), recursive=True):
+        print("Parsing file %s" % subm_file)
+        investments = parse_submission_file(subm_file)
+        if investments is None:
+            print("  Skip file, no xml blocks found!")
+            continue
+
+        lst += investments
+
+    return pd.DataFrame(lst)
+
+
+# Execute this code only when running the python file as program, not when importing
+# it from e.g. the unit test code.
+if __name__ == '__main__':
+    df = parse_all_13f_submission_files(root_folder="data/sec-edgar-filings")
+    df.to_excel("data/subset_submission_files.xlsx", index=False)
--- test/test_parse_submission_files.py
+++ test/test_parse_submission_files.py
@@ -1,7 +1,8 @@
 import logging
 import unittest
 
-from form_13f_parser import parse_submission_file, parse_investment
+from form_13f_parser import parse_submission_file, parse_investment_from_xml, \
+    parse_cid_from_table
 
 
 class TestParseSubmissionFile(unittest.TestCase):
@@ -12,24 +13,60 @@ class TestParseSubmissionFile(unittest.TestCase):
 
     def test_parse_vanguard_2019_submision(self):
         """
+        Submission file containing XML
         """
         file_name = "data/102909-full-submission.txt"
-        parsed_data = parse_submission_file(file_name)
-        # ET.dump(parsed_data)
+        investment_els = parse_submission_file(file_name)
 
-        # parsed_data is an xml ElementTree element, so we can use xpath to browse the
-        # tree
-        ns = { "ns1": "http://www.sec.gov/edgar/thirteenffiler"}
-        cik = parsed_data.find('.//ns1:edgarSubmission/ns1:headerData/ns1:filerInfo/ns1:filer/ns1:credentials/ns1:cik', ns).text
-        self.assertEqual("0000102909", cik)
-
-        # List the companies the investment fund is investing in
-        ns = {"ns1": "http://www.sec.gov/edgar/document/thirteenf/informationtable"}
-        investment_els = parsed_data.findall('.//ns1:informationTable/ns1:infoTable', ns)
+        self.assertTrue(isinstance(investment_els, list))
         self.assertEqual(13483, len(investment_els))
-        for investment_el in investment_els:
-            investment = parse_investment(investment_el)
-            print(investment)
 
-        pass
+        first_investment = investment_els[0]
+        self.assertTrue(isinstance(first_investment, dict))
+        self.assertEqual("0000102909", first_investment['cik'])
 
+    def test_parse_table_submision(self):
+        """
+        Submission file containing fixed-width table
+        """
+        file_name = "data/885709-12-10-no-xml-full-submission.txt"
+        investment_els = parse_submission_file(file_name)
+
+        self.assertTrue(isinstance(investment_els, list))
+        self.assertEqual(212, len(investment_els))
+
+        first_investment = investment_els[0]
+        self.assertTrue(isinstance(first_investment, dict))
+        self.assertEqual("0000885709", first_investment['cik'])
+
+    def test_parse_table_diff_layout_submision(self):
+        """
+        Submission file containing fixed-width table
+        """
+        file_name = "data/885709-11-55-no-xml-full-submission.txt"
+        investment_els = parse_submission_file(file_name)
+
+        self.assertTrue(isinstance(investment_els, list))
+        self.assertEqual(253, len(investment_els))
+
+        first_investment = investment_els[0]
+        self.assertTrue(isinstance(first_investment, dict))
+        self.assertEqual("0000885709", first_investment['cik'])
+
+    def test_parse_table_diff_layout2_submision(self):
+        """
+        Submission file containing fixed-width table
+        """
+        file_name = "data/885709-01-04-no-xml-full-submission.txt"
+        investment_els = parse_submission_file(file_name)
+
+        self.assertTrue(isinstance(investment_els, list))
+        self.assertEqual(79, len(investment_els))
+
+        first_investment = investment_els[0]
+        self.assertTrue(isinstance(first_investment, dict))
+        self.assertEqual("0000885709", first_investment['cik'])
+
+    def test_parse_table_cid_from_accession_number(self):
+        accession_number = "0000885709-12-000010"
+        self.assertEqual("0000885709", parse_cid_from_table(accession_number))
