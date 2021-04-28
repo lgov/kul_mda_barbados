0230a2dadb63ae88b280ec9bc7c5d404db83cecb
L-SXiong <shixiong.luo@ucdconnect.ie>
2021-04-28 17:34:05 +0200

    clustering method (potencial) - add two functions dataset_afterlabel_kmean (get the dataset of specific group) dataset_centroids (get the centroids dataset)

--- src/clustering.py
+++ src/clustering.py
@@ -7,6 +7,10 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 import numpy as np
 
+"""
+Summmary a date in a specific year with cid, number of stock, total value of stocks, 
+and then log the data for further analysis
+"""
 def data_number_total_log (path,year):
     df = pd.read_excel(path)
     data = df[df["report_end_date"].dt.year == year]
@@ -25,10 +29,15 @@ def data_number_total_log (path,year):
     data_final['total_value'] = numpy.log(data_final['total_value'])
     return  data_final
 
-
+"""
+Plot the dataset with regression line 
+"""
 def data_plot (data_afterlog):
     sns.lmplot(x="number_stocks", y="total_value", data=data_afterlog)
 
+"""
+show the optimize number of culsters
+"""
 def optimize_kmean (data_afterlog):
     wcss = []
     n=11
@@ -44,6 +53,39 @@ def optimize_kmean (data_afterlog):
     plt.xlabel('Number of clusters')
     plt.ylabel('WCSS')
 
+"""
+Label the label for kmean and we can use the label to find the dataset of specific cluster 
+eg. 
+x1=data_number_total_log("./data/all_submission_files.xlsx",2020)
+label=dataset_afterlabel_kmean(x1,4)
+x1[label==1]
+And then we can get the dataset of group 1
+"""
+def dataset_afterlabel_kmean(data_afterlog,clusters_n):
+    pca = PCA(2)
+    data_cluster = pca.fit_transform(data_afterlog)
+    kmeans = KMeans(n_clusters=clusters_n)
+    label = kmeans.fit_predict(data_cluster)
+    return label
+
+"""
+Get the centroids of kmean 
+eg. 
+x2=data_number_total_log("./data/all_submission_files.xlsx",2020)
+dataset_centroids(x2,4)
+And then we can get the centroids.
+"""
+def dataset_centroids(data_afterlog,clusters_n):
+    pca = PCA(2)
+    data_cluster = pca.fit_transform(data_afterlog)
+    kmeans = KMeans(n_clusters=clusters_n)
+    kmeans.fit_predict(data_cluster)
+    centroids = kmeans.cluster_centers_
+    return centroids
+
+"""
+plot the clustering plot 
+"""
 def kmean_plot (data_afterlog,clusters_n):
     pca = PCA(2)
     data_cluster = pca.fit_transform(data_afterlog)
