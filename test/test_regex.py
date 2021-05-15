59c08009e4553c65e23ed267867f470a004dda1e
Lieven Govaerts <lgo@mobsol.be>
2021-05-15 22:55:29 +0200

    Parse the foundation year of the company from the longBusinessSummary column. Add a unit test.

--- test/test_regex.py
+++ test/test_regex.py
@@ -1,112 +1,36 @@
 import logging
+import re
 import unittest
-from datetime import date
-
-from form_13f_parser import parse_submission_file, parse_date_from_table, parse_date_from_xml,\
-    parse_cid_from_table, parse_amount_times_1000
 
 
-class TestParseSubmissionFile(unittest.TestCase):
+class TestUseRegex(unittest.TestCase):
     def setUp(self):
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(message)s')
 
-
-    def test_parse_vanguard_2019_submision(self):
+    def test_parse_foundation_year_of_a_comp(self):
         """
-        Submission file containing XML
+        Extract the year a company was founded from the text.
         """
-        file_name = "data/102909-full-submission.txt"
-        investment_els = parse_submission_file(file_name)
 
-        self.assertTrue(isinstance(investment_els, list))
-        self.assertEqual(13483, len(investment_els))
-
-        first_investment = investment_els[0]
-        self.assertTrue(isinstance(first_investment, dict))
-        self.assertEqual("COM", first_investment['titleOfClass'])
-        self.assertEqual("0000102909", first_investment['cik'])
-        self.assertEqual(date(2019, 3, 31), first_investment['report_end_date'])
-        self.assertEqual(237000, first_investment['value'])
-        self.assertEqual("1347 PPTY INS HLDGS INC", first_investment['nameOfIssuer'])
-
-    def test_parse_table_submision(self):
+        text = """
+        The Sherwin-Williams Company develops, manufactures, distributes, and sells paints, coatings, and related products to professional, industrial, commercial, and retail customers. It operates in three segments: The Americas Group, Consumer Brands Group, and Performance Coatings Group. The Americas Group segment offers architectural paints and coatings, and protective and marine products, as well as OEM product finishes and related products for architectural and industrial paint contractors, and do-it-yourself homeowners. The Consumer Brands Group segment provides branded and private-label architectural paints, stains, varnishes, industrial products, wood finishes products, wood preservatives, applicators, corrosion inhibitors, aerosols, caulks, and adhesives to retailers and distributors. The Performance Coatings Group segment develops and sells industrial coatings for wood finishing and general industrial applications, automotive refinish products, protective and marine coatings, coil coatings, packaging coatings, and performance-based resins and colorants. It serves retailers, dealers, jobbers, licensees, and other third-party distributors through its branches and direct sales staff, as well as through outside sales representatives. The company has operations primarily in North and South America, the Caribbean, Europe, Asia, and Australia. As of December 31, 2021, it operated 4,774 company-operated stores. The Sherwin-Williams Company was founded in 1866 and is headquartered in Cleveland, Ohio.
         """
-        Submission file containing fixed-width table
+        m = re.search(r'was founded in (\d{4}).*', text)
+        self.assertIsNotNone(m)
+        self.assertEqual("1866", m.group(1))
+
+        text = """
+        The Home Depot, Inc. operates as a home improvement retailer. It operates The Home Depot stores that sell various building materials, home improvement products, building materials, lawn and garden products, and dÃ©cor products, as well as provide installation, home maintenance, and professional service programs to do-it-yourself and professional customers. The company also offers installation programs that include flooring, cabinets and cabinet makeovers, countertops, furnaces and central air systems, and windows; and professional installation in various categories sold through its stores and in-home sales programs, as well as acts as a general contractor to provide installation services to its do-it-for-me customers through third-party installers. In addition, it provides tool and equipment rental services. The company primarily serves homeowners; and professional renovators/remodelers, general contractors, handymen, property managers, building service contractors, and specialty tradesmen, such as electricians, plumbers, and painters. It also sells its products online. As of January 31, 2021, the company operated 2,296 retail stores in the United States, including the Commonwealth of Puerto Rico, and the territories of the U.S. Virgin Islands and Guam; Canada; and Mexico. The Home Depot, Inc. was incorporated in 1978 and is based in Atlanta, Georgia.
         """
-        file_name = "data/885709-12-10-no-xml-full-submission.txt"
-        investment_els = parse_submission_file(file_name)
+        m = re.search(r'was founded in (\d{4}).*|was incorporated in (\d{4}).*', text)
+        self.assertIsNotNone(m)
+        self.assertEqual("1978", m.group(2))
 
-        self.assertTrue(isinstance(investment_els, list))
-        self.assertEqual(212, len(investment_els))
-
-        first_investment = investment_els[0]
-        self.assertTrue(isinstance(first_investment, dict))
-        self.assertEqual("0000885709", first_investment['cik'])
-        self.assertEqual(date(2011,12,31), first_investment['report_end_date'])
-        self.assertEqual(1774000, first_investment['value'])
-        self.assertEqual("AT&T", first_investment['nameOfIssuer'])
-
-    def test_parse_table_diff_layout_submision(self):
+        text = """
+        Blackrock Resources & Commodities Strategy Trust is a closed-ended equity mutual fund launched by BlackRock, Inc. It is co-managed by BlackRock Advisors, LLC and BlackRock International Limited. The fund invests in the public equity markets of the United States. It seeks to invest in stocks of companies operating in the commodities or natural resources sectors. The fund also invests through derivatives with exposure to commodity or natural resources companies, with an emphasis on option writing. Blackrock Resources & Commodities Strategy Trust was formed on March 30, 2011 and is domiciled in the United States.
         """
-        Submission file containing fixed-width table
-        """
-        file_name = "data/885709-11-55-no-xml-full-submission.txt"
-        investment_els = parse_submission_file(file_name)
-
-        self.assertTrue(isinstance(investment_els, list))
-        self.assertEqual(253, len(investment_els))
-
-        first_investment = investment_els[0]
-        self.assertTrue(isinstance(first_investment, dict))
-        self.assertEqual("0000885709", first_investment['cik'])
-        self.assertEqual(date(2011,9,30), first_investment['report_end_date'])
-        self.assertEqual(35000, first_investment['value'])
-        self.assertEqual("3M COMPANY", first_investment['nameOfIssuer'])
-
-    def test_parse_table_diff_layout2_submision(self):
-        """
-        Submission file containing fixed-width table. Value x 1000
-        """
-        file_name = "data/885709-01-04-no-xml-full-submission.txt"
-        investment_els = parse_submission_file(file_name)
-
-        self.assertTrue(isinstance(investment_els, list))
-        self.assertEqual(79, len(investment_els))
-
-        first_investment = investment_els[0]
-        self.assertTrue(isinstance(first_investment, dict))
-        self.assertEqual("0000885709", first_investment['cik'])
-        self.assertEqual(date(2001,3,31), first_investment['report_end_date'])
-        self.assertEqual(160000, first_investment['value'])
-        self.assertEqual("ACACIA RESEARCH CORP", first_investment['nameOfIssuer'])
-
-    def test_parse_table_cid_from_accession_number(self):
-        accession_number = "0000885709-12-000010"
-        self.assertEqual("0000885709", parse_cid_from_table(accession_number))
-
-    def test_parse_table_date(self):
-        date_str = "20041026"
-        self.assertEqual(date(2004, 10, 26), parse_date_from_table(date_str))
-
-        # Test that whitespaces are stripped first.
-        date_str = " 20041026"
-        self.assertEqual(date(2004, 10, 26), parse_date_from_table(date_str))
-
-    def test_parse_xml_date(self):
-        date_str = "03-31-2019"
-        self.assertEqual(date(2019, 3, 31), parse_date_from_xml(date_str))
-
-        # Test that whitespaces are stripped first.
-        date_str = " 03-31-2019"
-        self.assertEqual(date(2019, 3, 31), parse_date_from_xml(date_str))
-
-    def test_parse_amount_times_1000(self):
-        value_str = "$   160"
-        self.assertEqual(160000, parse_amount_times_1000(value_str))
-
-        value_str = "--------"
-        self.assertIsNone(parse_amount_times_1000(value_str))
-
-        value_str = "1,774"
-        self.assertEqual(1774000, parse_amount_times_1000(value_str))
+        m = re.search(r'was formed on [A-Za-z]+ \d+, (\d{4}).*', text)
+        self.assertIsNotNone(m)
+        self.assertEqual("2011", m.group(1))
+        ''
